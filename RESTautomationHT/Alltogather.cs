194f7504b56b6262088676ce7ba01ec8701c507c
“kVadm” <1408vk@gmail.com>
2017-01-07 21:54:08 +0200

    run asyn

--- RESTautomationHT/Alltogather.cs
+++ RESTautomationHT/Alltogather.cs
@@ -3,12 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
 
 namespace RESTautomationHT
 {
+    #region ASP.NET example
+    class Program
+    {
+
+
+       
+        
+       
+        //static async Task<Product> GetProductAsync(string path)
+        //{
+        //    Product product = null;
+        //    HttpResponseMessage response = await client.GetAsync(path);
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        product = await response.Content.ReadAsAsync<Product>();
+        //    }
+        //    return product;
+        //}
+    }
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+    #endregion
+
+
     public class RESTclient
     {
-        //initializer
+        
+        
+
+        static HttpClient client = new HttpClient();
+       
+      
+
+        
+       
         //Switcher
         //Executor
     }
