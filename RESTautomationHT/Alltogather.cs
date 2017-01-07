e5459341b89b8a41458f822d12e35730c2ce4223
“kVadm” <1408vk@gmail.com>
2017-01-07 16:04:48 +0200

    1

---
+++ RESTautomationHT/Alltogather.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTautomationHT
+{
+    public class RESTclient
+    {
+        //initializer
+        //Switcher
+        //Executor
+    }
+
+      public class CRUDtests
+    {
+        RESTclient restclient = new RESTclient();
+
+        //create entity
+          public void CreateEntity(){
+
+          }
+        //rename entity
+        //update entity
+        //delete entity
+    }
+}
+}
