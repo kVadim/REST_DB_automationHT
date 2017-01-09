8d06343f4edabdabb9ed254f1ef82466cdb0d079
Vadim Kotliarchuk <1408vk@gmail.com>
2017-01-09 14:25:16 +0200

    9des

--- RESTautomationHT/Alltogather.cs
+++ RESTautomationHT/Alltogather.cs
@@ -29,14 +29,6 @@ namespace RESTautomationHT
         //    return product;
         //}
     }
-    public class Product
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public string Category { get; set; }
-    }
-    #endregion
 
 
     public class RESTclient
